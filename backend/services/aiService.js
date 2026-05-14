4fd8421a7d46796ac2efb21ce961147c2f58fae1
Lpei <lpei1900@gmail.com>
2026-05-13 21:11:08 -0400

    update gemini version

--- backend/services/aiService.js
+++ backend/services/aiService.js
@@ -4,7 +4,7 @@ const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
 const genAI = new GoogleGenerativeAI(GEMINI_API_KEY);
 
 const model = genAI.getGenerativeModel({
-    model: 'gemini-2.5',
+    model: 'gemini-2.5-flash',
     generationConfig: {
         maxOutputTokens: 350,
         temperature: 0.6,
